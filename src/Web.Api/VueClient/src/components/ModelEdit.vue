374675d81cbaf44cc024ee414cb62a9bc86b3923
Gabriela Rodriguez-Florido <gr64@duke.edu>
2020-02-03 22:14:39 -0500

    added frontend input validation for numbers in models and added spacing in model edit and add model dialogs

--- src/Web.Api/VueClient/src/components/ModelEdit.vue
+++ src/Web.Api/VueClient/src/components/ModelEdit.vue
@@ -6,7 +6,7 @@
             </v-card-title>
             <v-card-text>
             <v-container>
-                <v-row no-gutters>
+                <v-row>
                     <v-col cols="12" sm="6" md="4">
                         <v-combobox v-model="editedItem.vendor"
                                     :items="models"
@@ -18,19 +18,19 @@
                         <v-text-field v-model="editedItem.modelNumber" label="Model Number" counter="50"></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.height" label="Height"></v-text-field>
+                        <v-text-field v-model="editedItem.height" label="Height" type="number"></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.ethernetPorts" label="Ethernet Ports"></v-text-field>
+                        <v-text-field v-model="editedItem.ethernetPorts" label="Ethernet Ports" type="number"></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.powerPorts" label="Power Ports"></v-text-field>
+                        <v-text-field v-model="editedItem.powerPorts" label="Power Ports" type="number"></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
                         <v-text-field v-model="editedItem.cpu" label="CPU" counter="50"></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.memory" label="Memory"></v-text-field>
+                        <v-text-field v-model="editedItem.memory" label="Memory" type="number"></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
                         <v-text-field v-model="editedItem.storage" label="Storage"></v-text-field>
