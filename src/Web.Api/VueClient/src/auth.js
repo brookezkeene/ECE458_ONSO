5efceab92572025db30ba16dc13636e70f1e730d
Gabriela Rodriguez-Florido <gr64@duke.edu>
2020-02-03 20:19:15 -0500

    fixed hardcoded return true line

--- src/Web.Api/VueClient/src/auth.js
+++ src/Web.Api/VueClient/src/auth.js
@@ -31,8 +31,7 @@ export default {
     },
 
     isAdmin() {
-        return true;
-        //return localStorage.user && localStorage.user.admin;
+        return localStorage.user && localStorage.user.admin;
     },
 
 }
