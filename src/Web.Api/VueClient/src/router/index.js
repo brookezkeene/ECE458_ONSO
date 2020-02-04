087700c06ba9590cdb77caee1a42eb9638bac60e
unknown <jyz11@duke.edu>
2020-02-04 17:53:57 -0500

    added nav gaurds: beforeEnter functions to each page - from the auth.js, calls the loggedIn() function to verify user has logged in. otherwise returns user to login page

--- src/Web.Api/VueClient/src/router/index.js
+++ src/Web.Api/VueClient/src/router/index.js
@@ -9,6 +9,7 @@ import Instances from '@/components/Instances'
 import Racks from '@/components/Racks'
 import ImportExport from '@/components/Bulk'
 import Users from '@/components/Users'
+import Auth from '@/auth'
 
 Vue.use(Router)
 
@@ -16,51 +17,105 @@ const routes = [
     {
         path: '/',
         name: 'Welcome',
-        component: Login
+        component: Login,
     },
     {
         path: '/Models',
         name: 'model',
-        component: Models
+        component: Models,
+        beforeEnter(to, from, next) {
+            if (Auth.loggedIn()) {
+                next();
+            }
+            else next({ path: '/' });
+        }
     },
     {
         path: '/Models/:id',
         name: 'model-details',
         component: ModelDetails,
-        props: true
+        props: true,
+        beforeEnter(to, from, next) {
+            if (Auth.loggedIn()) {
+                next();
+            } 
+            else next({ path: '/' });
+        }
+
     },
     {
         path: '/Instances',
         name: 'instances',
-        component: Instances
+        component: Instances,
+        beforeEnter(to, from, next) {
+            if (Auth.loggedIn()) {
+                next();
+            }
+            else next({ path: '/' });
+        }
     },
     {
         path: '/Instances/:id',
         name: 'instance-details',
         component: InstanceDetails,
-        props: true
+        props: true,
+        beforeEnter(to, from, next) {
+            if (Auth.loggedIn()) {
+                next();
+            }
+            else next({ path: '/' });
+        }
     },
     {
         path: '/racks',
         name: 'racks',
         component: Racks,
+        beforeEnter(to, from, next) {
+            if (Auth.loggedIn()) {
+                next();
+            }
+            else next({ path: '/' });
+        }
     },
     {
         path: '/importexport',
         name: 'import-export',
         component: ImportExport,
+        beforeEnter(to, from, next) {
+            if (Auth.loggedIn()) {
+                next();
+            }
+            else next({ path: '/' });
+        }
     },
     {
         path: '/racks/view',
         name: 'RackDiagram',
-        component: RackDiagram
+        component: RackDiagram,
+        beforeEnter(to, from, next) {
+            if (Auth.loggedIn()) {
+                next();
+            }
+            else next({ path: '/' });
+        }
     },
     {
         path: '/users',
         name: 'users',
         component: Users,
+        beforeEnter(to, from, next) {
+            if (Auth.loggedIn()) {
+                next();
+            }
+            else next({ path: '/' });
+        }
     }
 ]
+
+/*this.Router.beforeEach((to, from, next) => {
+    if (!Auth.loggedIn()) next('/')
+    else next();
+})*/
 
 export default new Router({
     mode: 'history',
