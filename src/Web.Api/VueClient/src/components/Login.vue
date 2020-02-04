39fc68ccf6605443ab89534c3a3a66370249d35a
brookezkeene <brooke.z.keene@gmail.com>
2020-02-03 20:32:31 -0500

    added styling to login page

--- src/Web.Api/VueClient/src/components/Login.vue
+++ src/Web.Api/VueClient/src/components/Login.vue
@@ -1,34 +1,44 @@
 <template>
-    <v-card>
-        <v-card-title>
-          Please sign in to your account!
-        </v-card-title>
-        <v-form
-            ref="form"
-            v-model = "valid">
-            <v-text-field
-                v-model="username"
-                :rules="usernameRules"
-                label="Username"
-                required
-            ></v-text-field>
-            <v-text-field
-                v-model="password"
-                :rules="passwordRules"
-                :type="'password'"
-                label="Password"
-                required
-            ></v-text-field>
-        </v-form>
-        <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="validate"
-        >
-            Sign In
-        </v-btn>
-    </v-card>
+    <v-container fill-height fluid>
+        <v-row justify="center">
+            <v-img src="./example/hyposoft_logo.png"
+                   max-width="300px"></v-img>
+        </v-row>
+        
+        <v-row justify="center">
+            <v-card width="50%">
+                <v-card-title class="justify-center">
+                    Log In
+                </v-card-title>
+                <v-card-text class="justify-center">
+                    <v-form ref="form"
+                            v-model="valid">
+                        <v-container>
+                            <v-row>
+                                <v-text-field v-model="username"
+                                              :rules="usernameRules"
+                                              label="Username"
+                                              required></v-text-field>
+                            </v-row>
+                            <v-row>
+                                <v-text-field v-model="password"
+                                              :rules="passwordRules"
+                                              :type="'password'"
+                                              label="Password"
+                                              required></v-text-field>
+                            </v-row>
+                        </v-container>
+                    </v-form>
+                    <v-btn :disabled="!valid"
+                           color="primary"
+                           class="mr-4"
+                           @click="validate">
+                        Sign In
+                    </v-btn>
+                </v-card-text>
+            </v-card>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
