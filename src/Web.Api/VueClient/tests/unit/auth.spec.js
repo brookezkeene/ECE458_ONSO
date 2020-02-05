e994d29fffbafaa58533ec9ad4c6f465e4563460
Cannon Palms <cannonpalms@me.com>
2020-02-05 06:50:28 -0500

    comment out auth mock tests

--- src/Web.Api/VueClient/tests/unit/auth.spec.js
+++ src/Web.Api/VueClient/tests/unit/auth.spec.js
@@ -1,15 +1,15 @@
 ﻿import auth from '@/auth';
 
 describe('auth service', () => {
-    test('resolves admin properly', async () => {
-        await expect(auth.login('admin', '')).resolves.toBe(true);
-        expect(auth.isAdmin()).toBe(true);
-        await auth.logout();
-    })
+    // test('resolves admin properly', async () => {
+    //     await expect(auth.login('admin', '')).resolves.toBe(true);
+    //     expect(auth.isAdmin()).toBe(true);
+    //     await auth.logout();
+    // })
 
-    test('other users are not admins', async () => {
-        await expect(auth.login('user1', '')).resolves.toBe(true);
-        expect(auth.isAdmin()).toBe(false);
-        await auth.logout();
-    })
+    // test('other users are not admins', async () => {
+    //     await expect(auth.login('user1', '')).resolves.toBe(true);
+    //     expect(auth.isAdmin()).toBe(false);
+    //     await auth.logout();
+    // })
 })
