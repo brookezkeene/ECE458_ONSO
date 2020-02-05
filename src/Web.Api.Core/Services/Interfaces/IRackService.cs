229f1a1275b6d28930c867f4df089debc4803fe2
Cannon Palms <cannonpalms@me.com>
2020-02-05 01:56:08 -0500

    correct naming conflict

--- src/Web.Api.Core/Services/Interfaces/IRackService.cs
+++ src/Web.Api.Core/Services/Interfaces/IRackService.cs
@@ -4,8 +4,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Web.Api.Common;
 using Web.Api.Core.Dtos;
-using Web.Api.Core.Mappers;
-using Web.Api.Infrastructure.Repositories.Interfaces;
 
 namespace Web.Api.Core.Services.Interfaces
 {
@@ -15,40 +13,5 @@ namespace Web.Api.Core.Services.Interfaces
         Task<PagedList<FlatRackDto>> GetRacksAsync(string search, int page = 1, int pageSize = 10);
         Task CreateRacksAsync(RackRangeQuery query);
         Task DeleteRacksAsync(RackRangeQuery query);
-    }
-
-    public class RackService : IRackService
-    {
-        private readonly IRackRepository _rackRepository;
-
-        public RackService(IRackRepository rackRepository)
-        {
-            _rackRepository = rackRepository;
-        }
-
-        public async Task<List<RackDto>> GetRacksAsync(RackRangeQuery query)
-        {
-            query = query.ToUpper();
-            var racks = await _rackRepository.GetRacksInRangeAsync(query.StartRow, query.StartCol, query.EndRow, query.EndCol);
-            return racks.ToDto();
-        }
-
-        public async Task<PagedList<FlatRackDto>> GetRacksAsync(string search, int page = 1, int pageSize = 10)
-        {
-            var racks = await _rackRepository.GetRacksAsync(search, page, pageSize);
-            return racks.ToFlatDto();
-        }
-
-        public async Task CreateRacksAsync(RackRangeQuery query)
-        {
-            query = query.ToUpper();
-            await _rackRepository.CreateRacksInRangeAsync(query.StartRow, query.StartCol, query.EndRow, query.EndCol);
-        }
-
-        public async Task DeleteRacksAsync(RackRangeQuery query)
-        {
-            query = query.ToUpper();
-            await _rackRepository.DeleteRacksInRangeAsync(query.StartRow, query.StartCol, query.EndRow, query.EndCol);
-        }
     }
 }
