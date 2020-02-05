a174d4334ca5405a828b0d7db161c1a9ce7a22d8
brookezkeene <brooke.z.keene@gmail.com>
2020-02-05 02:21:03 -0500

    moved old auth.js to mock

---
+++ src/Web.Api/VueClient/src/repositories/mock/auth.js
@@ -0,0 +1,49 @@
+﻿/* globals localStorage */
+
+export default {
+    login(username, pass) {
+        if (localStorage.token) {
+            return Promise.resolve(true);
+        }
+        return pretendRequest(username, pass).then(
+            (res) => {
+                if (res.authenticated) {
+                    localStorage.token = res.token;
+                    localStorage.user = res.user;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+    },
+
+    getToken() {
+        return localStorage.token;
+    },
+
+    logout() {
+        delete localStorage.token;
+        return Promise.resolve();
+    },
+
+    loggedIn() {
+        //return true;
+        return !!localStorage.token;
+    },
+
+    isAdmin() {
+        //return true;
+        return localStorage.user && localStorage.user.admin;
+    },
+
+}
+
+function pretendRequest(username) {
+    return Promise.resolve({
+        authenticated: true,
+        token: Math.random().toString(36).substring(7),
+        user: {
+            admin: username === 'admin'
+        }
+    })
+}
