d3ab7b0f721e42c8c2f3f83761eabc698a032cef
Gabriela Rodriguez-Florido <gaby.rfp@gmail.com>
2020-02-07 11:59:37 -0500

    fixed name of vendor being blank or object, but still can't add more than one model

--- src/Web.Api/VueClient/src/components/ModelEdit.vue
+++ src/Web.Api/VueClient/src/components/ModelEdit.vue
@@ -5,59 +5,126 @@
             <span class="headline">Edit</span>
             </v-card-title>
             <v-card-text>
-            <v-container>
-                <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.vendor" label="Vendor" counter="50"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.modelNumber" label="Model Number" counter="50"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model.number="editedItem.height" label="Height" type="number"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model.number="editedItem.ethernetPorts" label="Ethernet Ports" type="number"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model.number="editedItem.powerPorts" label="Power Ports" type="number"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.cpu" label="CPU" counter="50"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model.number="editedItem.memory" label="Memory" type="number"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.storage" label="Storage"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.comment" label="Comment" multiLine textarea></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.displayColor" label="Display Color"></v-text-field>
-                    </v-col>
-                </v-row>
-            </v-container>
+
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-combobox v-model="newItem.vendor"
+                                        :items="models"
+                                        item-text="vendor"
+                                        item-value=""
+                                        :return-object="false"
+                                        label="Vendor"
+                                        counter="50"
+                                        clearable></v-combobox>
+                        </v-col>
+                        <v-col>
+                        <v-text-field v-model="newItem.modelNumber" label="Model Number" counter="50"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model.number="newItem.height" label="Height" type="number"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model.number="newItem.ethernetPorts" label="Ethernet Ports" type="number"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model.number="newItem.powerPorts" label="Power Ports" type="number"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="newItem.cpu" label="CPU" counter="50"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model.number="newItem.memory" label="Memory" type="number"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="newItem.storage" label="Storage"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="newItem.comment" label="Comment" multiLine textarea></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-label>
+                                Display Color
+                            </v-label>
+                            <v-color-picker v-model="newItem.displayColor">
+                            </v-color-picker>
+                        </v-col>
+                    </v-row>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn text @click="close">Cancel</v-btn>
+                        <v-btn color="primary" text @click="save">Save</v-btn>
+                    </v-card-actions>'
+
+                </v-container>
             </v-card-text>
         </v-card>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'model-edit',
-    inject: ['modelRepository'],
-    item: null,
-    props: {
-        editedItem : Object,
-        models: Array,
-    },
-    data () {
-        return {
-            loading: false,
-        };
-    },
-    editedIndex: -1,
-}
+    export default {
+        name: 'model-edit',
+        inject: ['modelRepository'],
+        props: {
+            id: String,
+        },
+        data() {
+            return {
+                models: [],
+                loading: false,
+                newItem: {
+                    vendor: '',
+                    modelNumber: '',
+                    height: 0,
+                    displayColor: '',
+                    ehternetPorts: 0,
+                    powerPorts: 0,
+                    cpu: '',
+                    memory: undefined,
+                    storage: '',
+                    comment: ''
+                },
+                isNew: 'false',
+
+            };
+        },
+        editedIndex: -1,
+
+        async created() {
+            this.models = await this.modelRepository.list();
+
+            this.newItem = typeof this.id === 'undefined'
+                ? this.newItem
+                : this.models.find(o => o.id === this.id);
+
+        },
+
+        computed: {
+            formTitle() {
+                return this.isNew ? 'New Item' : 'Edit Item'
+            },
+        },
+        methods: {
+            save() {
+                /*eslint-disable no-console */
+                if (!this.isNew) {
+                    console.log(this.newItem.vendor);
+                    console.log(this.newItem.displayColor);
+                    this.modelRepository.update(this.newItem);
+                } else {
+                    console.log(this.newItem.vendor);
+                    console.log(this.newItem.displayColor);
+                    this.modelRepository.create(this.newItem);
+                }
+                this.close()
+            },
+            close() {
+                setTimeout(() => {
+                    this.$router.push({ name: 'model' })
+                }, 300)
+            },
+        }
+    }
 </script>
