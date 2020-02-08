2a203971e5178ede1d3658561024a8217ab69f96
unknown <jyz11@duke.edu>
2020-02-07 19:13:29 -0500

    created ExportQuery and ExportController; filling in slowly

---
+++ src/Web.Api.Core/Dtos/ExportQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Web.Api.Core.Dtos
+{
+    public class ExportQuery : RackRangeQuery
+    {
+        [Required]
+        public string Search { get; set; }
+
+    }
+    public static class ExportQueryExtensions
+    {
+        public static ExportQuery ToUpper(this ExportQuery query)
+        {
+            var upperQuery = new ExportQuery { StartCol = query.StartCol, EndCol = query.EndCol };
+            upperQuery.StartRow = query.StartRow.ToUpper();
+            upperQuery.EndRow = query.EndRow.ToUpper();
+            upperQuery.Search = query.Search.ToUpper();
+            return upperQuery;
+        }
+    }
+}
