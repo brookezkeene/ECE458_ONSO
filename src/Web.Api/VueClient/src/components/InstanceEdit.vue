e5a9282be21131a4c0bca81736108bce443d0341
Cannon Palms <cannonpalms@me.com>
2020-02-09 23:51:27 -0500

    resolve issues with creating/editing multiple instances & using spaces in vendor/modelNumber

--- src/Web.Api/VueClient/src/components/InstanceEdit.vue
+++ src/Web.Api/VueClient/src/components/InstanceEdit.vue
@@ -8,12 +8,11 @@
                 <v-container>
                     <v-row>
                         <v-col cols="12" sm="6" md="4">
-                            <v-autocomplete v-model="editedItem.model.vendorModelNo"
+                            <v-autocomplete v-model="editedItem.model.id"
                                             :items="models"
                                             item-text="vendorModelNo"
-                                            item-value="vendorModelNo"
-                                            label="Model"
-                                            :return-object="false">
+                                            item-value="id"
+                                            label="Model">
                             </v-autocomplete>
                         </v-col>
                         <v-col cols="12" sm="6" md="4">
@@ -29,13 +28,12 @@
                             <v-text-field v-model="editedItem.rackPosition" label="Rack Position" type="number"></v-text-field>
                         </v-col>
                         <v-col cols="12" sm="6" md="4">
-                            <v-autocomplete v-model="newOwner"
+                            <v-autocomplete v-model="editedItem.owner.id"
                                             :items="users"
                                             item-text="username"
-                                            item-value="username"
+                                            item-value="id"
                                             label="Owner User Name"
-                                            persistent-hint
-                                            :return-object="false">
+                                            persistent-hint>
                             </v-autocomplete>
                         </v-col>
                         <v-col cols="12" sm="6" md="4">
@@ -64,33 +62,6 @@
                 users: [],
                 instances: [],
                 loading: false,
-                newOwner: "",
-                item: {
-                        model: {
-                            id: '',
-                            vendor: '',
-                            modelNumber: '',
-                            height: 0,
-                            displayColor: '',
-                            ethernetPorts: 0,
-                            powerPorts: 0,
-                            cpu: '',
-                            memory: '',
-                            storage: '',
-                            vendorModelNo: ''
-                        },
-                        hostname: '',
-                        rack: '',
-                        owner: {
-                            id: '',
-                            username: '',
-                            firstName: '',
-                            lastName: '',
-                            email: '',
-                        },
-                        rackPosition: '',
-                        comment: ''
-                },
                 editedItem: {
                   model: {
                     id: '',
@@ -124,10 +95,10 @@
             this.models = await this.modelRepository.list();
             this.users = await this.userRepository.list();
             this.instances = await this.instanceRepository.list();
-            if (this.instances.find(o => o.id === this.id) != undefined) {
-                this.editedItem = await this.instances.find(o => o.id === this.id);
-            } else {
-                this.editemItem = Object.assign({}, this.item);
+
+            const existingItem = this.instances.find(o => o.id == this.id);
+            if (typeof existingItem !== 'undefined') {
+                this.editedItem = Object.assign({}, existingItem);
             }
 
             for (const model of this.models) {
@@ -138,55 +109,30 @@
         computed: {
             formTitle() {
                 return this.isNew ? 'New Item' : 'Edit Item'
-            },
+            }
         },
         methods: {
             save() {
 
-                this.updateOwner(this.newOwner, this.editedItem);
-                this.updateModel(this.editedItem.model.vendorModelNo, this.editedItem);
+                this.updateOwner();
+                this.updateModel();
 
                 if (!this.isNew) {
-                    this.instanceRepository.update(this.editedItem);
+                    this.instanceRepository.update(this.editedItem).then(this.close);
                 } else {
-                    this.instanceRepository.create(this.editedItem);
+                    this.instanceRepository.create(this.editedItem).then(this.close);
                 }
-                this.close()
             },
             close() {
-                setTimeout(() => {
-                    this.$router.push({ name: 'instances'})
-                }, 300)
+                this.$router.push({ name: 'instances'})
             },
-            updateOwner(newUser, item) {
-
-                for (const user of this.users) {
-                                    /*eslint-disable*/
-                    console.log(newUser);
-                    if (newUser === user.username) {
-                        console.log(newUser);
-                        item.owner.username = newUser;
-                        item.owner.firstName = user.firstName;
-                        item.owner.lastName = user.lastName;
-                        item.owner.email = user.email;
-                    }
-                }
-                return item;
+            updateOwner() {
+                const userId = this.editedItem.owner.id;
+                this.editedItem.owner = this.users.find(o => o.id === userId);
             },
-            updateModel(newModel, item) {
-
-                for (const model of this.models) {
-                /*eslint-disable*/
-                    console.log(newModel)
-
-                    /*Potential Bug if have a model, modelNo combo with more than one space*/
-                    let val = newModel.split(" ")
-
-                    if (val[0] === model.vendor && val[1] === model.modelNumber) {
-                        item.model = model;
-                    }
-                }
-                return item;
+            updateModel() {
+                const modelId = this.editedItem.model.id;
+                this.editedItem.model = this.models.find(o => o.id == modelId);
             }
         }
     }
--- src/Web.Api/VueClient/src/repositories/mock/instance.js
+++ src/Web.Api/VueClient/src/repositories/mock/instance.js
@@ -1,87 +1,96 @@
-const mockInstances = [
-	{
-		"id": 1,
-		"model": {
-			"id": 1,
-			"vendor": "Dell",
-			"modelNumber": "R710",
-			"height": 4,
-			"displayColor": "#82E0AA",
-			"ethernetPorts": 2,
-			"powerPorts": 1,
-			"cpu": "Intel Xeon E5520 2.2GHz",
-			"memory": 32,
-			"storage": "2x500GB SSD RAID1",
-			"comment": "Retired offering, no new purchasing"
-		},
-		"hostname": "server9",
-		"rack": "B12",
-		"rackPosition": 5,
-		"owner": {
-			"id": 1,
-			"username": "tbletsch",
-			"firstName": "Tyler",
-			"lastName":"Bletsch",
-			"email": "Tyler.Bletsch@duke.edu",
-		},
-		"comment": "Reserved for Palaemon project"
-	},
-	{
-		"id": 2,
-		"model": {
-            "id": 2,
-			"vendor": "Lenovo",
-			"modelNumber": "Foobar",
-			"height": 3,
-			"displayColor": "#C39BD3",
-            "ethernetPorts": 2,
-			"powerPorts": 1,
-			"cpu": "Intel Xeon E5520 2.2GHz",
-			"memory": 32,
-			"storage": "2x500GB SSD RAID1",
-			"comment": "This is my personal favorite!",
-			"instances": ["instance3", "instance4", "instance5"]
-		},
-		"hostname": "server10",
-		"rack": "B15",
-		"rackPosition": 9,
-		"owner": {
-			"id": 1,
-			"username": "gabyrfp",
-			"firstName": "Gaby",
-			"lastName": "Rodriguez-Florido",
-			"email": "gr64@duke.edu",
-		},
-		"comment": "Reserved for 458 project"
-	},
-	{
-		"id": 3,
-		"model": {
-			"id": 2,
-			"vendor": "Lenovo",
-			"modelNumber": "Foobar",
-			"height": 4,
-			"displayColor": "#C39BD3",
-			"ethernetPorts": 2,
-			"powerPorts": 1,
-			"cpu": "Intel Xeon E5520 2.2GHz",
-			"memory": 32,
-			"storage": "2x500GB SSD RAID1",
-			"comment": "Retired offering, no new purchasing"
-		},
-		"hostname": "server5",
-		"rack": "B13",
-		"rackPosition": 1,
-		"owner": {
-			"id": 2,
-			"username": "cpalms",
-			"firstName": "Cannon",
-			"lastName": "Palms",
-			"email": "Cannon.Palms@duke.edu",
-		},
-		"comment": "Reserved for Palaemon project"
-	}
-];
+//const mockInstances = [
+//	{
+//		"id": 1,
+//		"model": {
+//			"id": 1,
+//			"vendor": "Dell",
+//			"modelNumber": "R710",
+//			"height": 4,
+//			"displayColor": "#82E0AA",
+//			"ethernetPorts": 2,
+//			"powerPorts": 1,
+//			"cpu": "Intel Xeon E5520 2.2GHz",
+//			"memory": 32,
+//			"storage": "2x500GB SSD RAID1",
+//			"comment": "Retired offering, no new purchasing"
+//		},
+//		"hostname": "server9",
+//		"rack": "B12",
+//		"rackPosition": 5,
+//		"owner": {
+//			"id": 1,
+//			"username": "tbletsch",
+//			"firstName": "Tyler",
+//			"lastName":"Bletsch",
+//			"email": "Tyler.Bletsch@duke.edu",
+//		},
+//		"comment": "Reserved for Palaemon project"
+//	},
+//	{
+//		"id": 2,
+//		"model": {
+//            "id": 2,
+//			"vendor": "Lenovo",
+//			"modelNumber": "Foobar",
+//			"height": 3,
+//			"displayColor": "#C39BD3",
+//            "ethernetPorts": 2,
+//			"powerPorts": 1,
+//			"cpu": "Intel Xeon E5520 2.2GHz",
+//			"memory": 32,
+//			"storage": "2x500GB SSD RAID1",
+//			"comment": "This is my personal favorite!",
+//			"instances": ["instance3", "instance4", "instance5"]
+//		},
+//		"hostname": "server10",
+//		"rack": "B15",
+//		"rackPosition": 9,
+//		"owner": {
+//			"id": 1,
+//			"username": "gabyrfp",
+//			"firstName": "Gaby",
+//			"lastName": "Rodriguez-Florido",
+//			"email": "gr64@duke.edu",
+//		},
+//		"comment": "Reserved for 458 project"
+//	},
+//	{
+//		"id": 3,
+//		"model": {
+//			"id": 2,
+//			"vendor": "Lenovo",
+//			"modelNumber": "Foobar",
+//			"height": 4,
+//			"displayColor": "#C39BD3",
+//			"ethernetPorts": 2,
+//			"powerPorts": 1,
+//			"cpu": "Intel Xeon E5520 2.2GHz",
+//			"memory": 32,
+//			"storage": "2x500GB SSD RAID1",
+//			"comment": "Retired offering, no new purchasing"
+//		},
+//		"hostname": "server5",
+//		"rack": "B13",
+//		"rackPosition": 1,
+//		"owner": {
+//			"id": 2,
+//			"username": "cpalms",
+//			"firstName": "Cannon",
+//			"lastName": "Palms",
+//			"email": "Cannon.Palms@duke.edu",
+//		},
+//		"comment": "Reserved for Palaemon project"
+//	}
+//];
+
+const uuidv4 = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const mockInstances = [];
 
 export default {
     find(id) {
@@ -95,9 +104,10 @@ export default {
 		return Promise.resolve(mockInstances);
     },
     create(item) {
-		if (mockInstances.find(o => o.id === item.id))
-            return Promise.reject();
+		if (typeof item.id !== 'undefined' && typeof mockInstances.find(o => o.id === item.id) !== 'undefined')
+            return Promise.reject(); // create() called on an existing item -> reject
 
+        item.id = uuidv4();
         mockInstances.push(item);
         return Promise.resolve();
     },
