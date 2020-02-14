88737aced42f46a09e6dea35e41c5f8055e8f9b2
Brooke Keene <brooke.z.keene@gmail.com>
2020-02-14 14:41:01 -0500

    axios calls for create, delete, get working, but bug in viewing rack diagrams

--- src/Web.Api/VueClient/src/service-container.js
+++ src/Web.Api/VueClient/src/service-container.js
@@ -1,7 +1,7 @@
 import userRepository from "./repositories/user";
 import modelRepository from "./repositories/model";
 import instanceRepository from "./repositories/mock/instance";
-import rackRepository from "./repositories/mock/rack";
+import rackRepository from "./repositories/rack";
 import reportRepository from "./repositories/mock/report";
 import auth from './auth';
 
