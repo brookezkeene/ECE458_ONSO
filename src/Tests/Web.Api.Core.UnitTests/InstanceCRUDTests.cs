42ef3bb37f7341ca4998be963841e0b68019c041
Joyce Zhou <jyz11@duke.edu>
2020-02-15 12:48:32 -0500

    got rid of build errors

--- src/Tests/Web.Api.Core.UnitTests/InstanceCRUDTests.cs
+++ src/Tests/Web.Api.Core.UnitTests/InstanceCRUDTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Web.Api.Core.Dtos;
-using Web.Api.Core.Resources;
 using Web.Api.Core.Services;
 using Web.Api.Core.UnitTests.Mappers;
 using Web.Api.Infrastructure.DbContexts;
