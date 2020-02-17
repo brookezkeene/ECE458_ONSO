27be59fea6b7de63649aefc56869d1264edd9aff
Brooke Keene <brooke.z.keene@gmail.com>
2020-02-16 21:18:43 -0500

    added mock API layer for datacenters

---
+++ src/Web.Api/VueClient/src/repositories/mock/datacenter.js
@@ -0,0 +1,53 @@
+﻿const mockDatacenters = [
+	{
+		"id": 1,
+		"abbreviation": "RTP1",
+		"name": "Research Triangle Park lab 1"
+	},
+	{
+		"id": 2,
+		"abbreviation": "RTP2",
+		"name": "Research Triangle Park lab 2"
+	},
+	{
+		"id": 3,
+		"abbreviation": "SV1",
+		"name": "Silicon Valley lab 1"
+	},
+];
+
+export default {
+	find(id) {
+		const item = mockDatacenters.find(o => o.id === id);
+		if (item)
+			return Promise.resolve(item);
+
+		return Promise.reject();
+	},
+	list() {
+		return Promise.resolve(mockDatacenters);
+	},
+	create(item) {
+		if (mockDatacenters.find(o => o.id === item.id))
+			return Promise.reject();
+
+		mockDatacenters.push(item);
+		return Promise.resolve();
+	},
+	update(item) {
+		const index = mockDatacenters.findIndex(o => o.id === item.id);
+		if (index < 0)
+			return Promise.reject();
+
+		mockDatacenters[index] = item;
+		return Promise.resolve();
+	},
+	delete(item) {
+		const index = mockDatacenters.findIndex(o => o.id === item.id);
+		if (index < 0)
+			return Promise.reject();
+
+		mockDatacenters.splice(index, 1);
+		return Promise.resolve();
+	}
+};
