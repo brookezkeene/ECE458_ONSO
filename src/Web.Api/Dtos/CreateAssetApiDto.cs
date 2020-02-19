72fd7f6a64467f786e7eec5117aca59fd76dac3f
Cannon Palms <cannonpalms@me.com>
2020-02-19 03:43:52 -0500

    update write endpoints for assets

--- src/Web.Api/Dtos/CreateAssetApiDto.cs
+++ src/Web.Api/Dtos/CreateAssetApiDto.cs
@@ -13,6 +13,20 @@ namespace Web.Api.Dtos
         public int RackPosition { get; set; }
         public Guid OwnerId { get; set; }
         public Guid ModelId { get; set; }
+        public int? AssetNumber { get; set; }
+        public List<CreateAssetPowerPortApiDto> PowerPorts { get; set; }
+        public List<CreateAssetNetworkPortApiDto> NetworkPorts { get; set; }
     }
 
+    public class CreateAssetNetworkPortApiDto
+    {
+        public string MacAddress { get; set; }
+        public Guid? ConnectedPortId { get; set; }
+    }
+
+    public class CreateAssetPowerPortApiDto
+    {
+        public int Number { get; set; }
+        public Guid? PduPortId { get; set; }
+    }
 }
