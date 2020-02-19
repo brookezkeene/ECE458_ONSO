ee3fa5621a8a91f59275ecf39e9501b2e398d042
Brooke Keene <brooke.z.keene@gmail.com>
2020-02-18 21:10:56 -0500

    updated racks table to rely on datacenter filter

--- src/Web.Api/VueClient/src/components/RackTable.vue
+++ src/Web.Api/VueClient/src/components/RackTable.vue
@@ -3,17 +3,27 @@
         <v-data-table
           :headers="filteredHeaders"
           :items="racks"
+          multi-sort
           >
             <template v-slot:top>
 
-            <v-toolbar flat color="white">
-                <v-toolbar-title>Existing Racks</v-toolbar-title>
-                <v-divider
-                class="mx-4"
-                inset
-                vertical
-                ></v-divider>
-            </v-toolbar>
+                <v-toolbar flat color="white">
+                    <v-toolbar-title>Existing Racks from </v-toolbar-title>
+
+                    <v-select v-model="selectedDatacenter"
+                              :items="datacenters"
+                              item-text="name"
+                              item-value=""
+                              :return-object="false"
+                              label="Datacenter"
+                              placeholder="Select a datacenter or all datacenters"
+                              class="pt-8 pl-4"
+                              clearable
+                              @change="datacenterSearch()">
+                    </v-select>
+                    <v-spacer></v-spacer>
+                    <v-spacer></v-spacer>
+                </v-toolbar>
 
             </template>
 
@@ -36,21 +46,21 @@
 
     export default {
         name: 'rack-table',
-        inject: ['rackRepository'],
+        inject: ['rackRepository', 'datacenterRepository'],
         item: null,
-        props: {
-            editedItem:Object
-        },
         data () {
             return {
                 loading: true,
                 headers: [
                     { text: 'Address', value: 'address'},
-                    { text: 'Row Letter', value: 'position.row' },
-                    { text: 'Rack Number', value: 'position.column' },
+                    { text: 'Row Letter', value: 'rowLetter' },
+                    { text: 'Rack Number', value: 'rackNumber' },
+                    { text: 'Datacenter', value: 'address' }, // change this to rack's datacenter field
                     { text: 'Actions', value: 'action', sortable: false },
                 ],
                 racks: [],
+                datacenters: [],
+                selectedDatacenter: ''
             };
         },
         computed: {
@@ -65,12 +75,26 @@
             this.initialize()
         },
         methods: {
-            async initialize () {
+            async initialize() {
                 this.racks = await this.rackRepository.list();
+                this.datacenters = await this.datacenterRepository.list();
+                var datacenter = {
+                    name: "All Datacenters",
+                    abbreviation: "All",
+                }
+                this.datacenters.push(datacenter);
                 this.loading = false;
             },
             deleteItem (item) {
                 confirm('Are you sure you want to delete this item?') && this.rackRepository.deleteInRange(item.address, item.address)
+            },
+            async datacenterSearch() {
+                if (this.selectedDatacenter === "All Datacenters") {
+                    // make special request for all datacenter racks
+                } else {
+                    this.racks = await this.rackRepository.list() // re-call based on new datacenter Name
+
+                }
             },
         }
     }
