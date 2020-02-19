a642a97cac4dd22f5269ad637d76491cee33fd76
Cannon Palms <cannonpalms@me.com>
2020-02-19 18:20:48 -0500

    add endpoints for asset power management

---
+++ src/Web.Api/Dtos/GetAssetPowerStateApiDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web.Api.Dtos
+{
+    public class GetAssetPowerStateApiDto
+    {
+        public Guid Id { get; set; } // asset id
+        public List<GetAssetPowerPortStateApiDto> PowerPorts { get; set; }
+    }
+}
