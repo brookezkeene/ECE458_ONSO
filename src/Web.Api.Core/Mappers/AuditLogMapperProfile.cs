f94641a97ff25ffdd80530aaad4c4b30c804390c
Cannon Palms <cannonpalms@me.com>
2020-02-23 01:29:50 -0500

    add audit log endpoint

---
+++ src/Web.Api.Core/Mappers/AuditLogMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Skoruba.AuditLogging.EntityFramework.Entities;
+using Web.Api.Common;
+using SkorubaCommon = Skoruba.AuditLogging.EntityFramework.Helpers.Common;
+using Web.Api.Common.Mappers;
+using Web.Api.Core.Dtos;
+
+namespace Web.Api.Core.Mappers
+{
+    public class AuditLogMapperProfile : PaginatedProfile
+    {
+        public AuditLogMapperProfile()
+        {
+            CreateMap(typeof(SkorubaCommon.PagedList<>), typeof(PagedList<>), MemberList.Source);
+            CreateMap<AuditLog, AuditLogDto>();
+        }
+    }
+}
---
+++ src/Web.Api.Core/Services/AuditLogService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Skoruba.AuditLogging.EntityFramework.Entities;
+using Skoruba.AuditLogging.EntityFramework.Repositories;
+using Web.Api.Common;
+using Web.Api.Core.Dtos;
+using Web.Api.Core.Mappers;
+using Web.Api.Core.Services.Interfaces;
+
+namespace Web.Api.Core.Services
+{
+    public class AuditLogService : IAuditLogService
+    {
+        private readonly IAuditLoggingRepository<AuditLog> _repository;
+
+        public AuditLogService(IAuditLoggingRepository<AuditLog> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<PagedList<AuditLogDto>> GetAsync(string search, int page = 1, int pageSize = 10)
+        {
+            var list = await _repository.GetAsync(page, pageSize);
+
+            var pagedList = list.MapTo<PagedList<AuditLogDto>>();
+            pagedList.CurrentPage = page;
+
+            return pagedList;
+        }
+    }
+}
