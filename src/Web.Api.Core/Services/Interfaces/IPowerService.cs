2f0b85fc0de3ba189150f13c2df14b265c323dd8
gabyr <gaby.rfp@gmail.com>
2020-02-25 00:27:57 -0500

    working on finding all the power port states for an asset, will not compile

---
+++ src/Web.Api.Core/Services/Interfaces/IPowerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Api.Core.Dtos;
+using Web.Api.Core.Dtos.Power;
+
+namespace Web.Api.Core.Services.Interfaces
+{
+    interface IPowerService
+    {
+        public Task<AssetPowerStateDto> setState(Guid assetId, PowerState state);
+
+        public Task<AssetPowerStateDto> getStates(Guid assetId);
+
+    }
+}
