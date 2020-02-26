2872895453ea4c88f6189836b40101df6e70aee4
Brooke Keene <brooke.z.keene@gmail.com>
2020-02-26 16:34:44 -0500

    added mocked repository for rack usage report

--- src/Web.Api/VueClient/src/repositories/mock/report.js
+++ src/Web.Api/VueClient/src/repositories/mock/report.js
@@ -3,9 +3,9 @@
 const mockRacks = [
     {
         "id": 1,
-        "address": "B12",
-        "rowLetter": "B",
-        "rackNumber": 12,
+        "address": "A1",
+        "rowLetter": "A",
+        "rackNumber": 1,
         "instances": [
             {
                 "id": 1,
@@ -13,23 +13,23 @@ const mockRacks = [
                     "id": 1,
                     "vendor": "Dell",
                     "modelNumber": "R710",
-                    "height": 4,
+                    "height": 2,
                     "displayColor": "#82E0AA",
-                    "ethernetPorts": 2,
-                    "powerPorts": 1,
+                    "ethernetPorts": 5,
+                    "powerPorts": 2,
                     "cpu": "Intel Xeon E5520 2.2GHz",
                     "memory": 32,
                     "storage": "2x500GB SSD RAID1",
                     "comment": "Retired offering, no new purchasing"
                 },
                 "hostname": "server1",
-                "rack": "B12",
-                "rackPosition": 4,
+                "rack": "A1",
+                "rackPosition": 1,
                 "owner": {
                     "id": 1,
-                    "username": "tbletsch",
-                    "displayName": "Tyler Bletsch",
-                    "email": "Tyler.Bletsch@duke.edu",
+                    "username": "jzhou",
+                    "displayName": "Joyce Zhou",
+                    "email": "jyz11@duke.edu",
                 },
                 "comment": "Reserved for Palaemon project"
             },
@@ -37,26 +37,25 @@ const mockRacks = [
                 "id": 2,
                 "model": {
                     "id": 2,
-                    "vendor": "Dell",
-                    "modelNumber": "R700",
-                    "height": 8,
+                    "vendor": "Cisco",
+                    "modelNumber": "SG112",
+                    "height": 1,
                     "displayColor": "#C39BD3",
-                    "ethernetPorts": 2,
-                    "powerPorts": 1,
+                    "ethernetPorts": 24,
+                    "powerPorts": 2,
                     "cpu": "Intel Xeon E5520 2.2GHz",
                     "memory": 32,
                     "storage": "2x500GB SSD RAID1",
                     "comment": "This is my personal favorite!",
-                    "instances": ["instance3", "instance4", "instance5"]
                 },
-                "hostname": "server2",
-                "rack": "B12",
-                "rackPosition": 12,
+                "hostname": "switch1",
+                "rack": "A1",
+                "rackPosition": 4,
                 "owner": {
                     "id": 1,
-                    "username": "grodriguez",
-                    "displayName": "Gaby Rodriguez-Florido",
-                    "email": "gr64@duke.edu",
+                    "username": "admin",
+                    "displayName": "Admin User",
+                    "email": "admin@test.com",
                 },
                 "comment": "Reserved for 458 project"
             },
@@ -65,142 +64,29 @@ const mockRacks = [
                 "model": {
                     "id": 1,
                     "vendor": "Dell",
-                    "modelNumber": "R710",
-                    "height": 4,
+                    "modelNumber": "R630",
+                    "height": 1,
                     "displayColor": "#82E0AA",
-                    "ethernetPorts": 2,
-                    "powerPorts": 1,
+                    "ethernetPorts": 4,
+                    "powerPorts": 2,
                     "cpu": "Intel Xeon E5520 2.2GHz",
                     "memory": 32,
                     "storage": "2x500GB SSD RAID1",
                     "comment": "Retired offering, no new purchasing"
                 },
-                "hostname": "server1",
-                "rack": "B12",
-                "rackPosition": 4,
+                "hostname": "server2",
+                "rack": "A1",
+                "rackPosition": 6,
                 "owner": {
                     "id": 1,
-                    "username": "jzhou",
-                    "displayName": "Joyce Zhou",
-                    "email": "Joyce.Zhou@duke.edu",
+                    "username": "bzk2",
+                    "displayName": "Brooke Keene",
+                    "email": "brooke.keene@duke.edu",
                 },
                 "comment": "Reserved for Duke project"
-            },
-            {
-                "id": 4,
-                "model": {
-                    "id": 1,
-                    "vendor": "Dell",
-                    "modelNumber": "R710",
-                    "height": 4,
-                    "displayColor": "#82E0AA",
-                    "ethernetPorts": 2,
-                    "powerPorts": 1,
-                    "cpu": "Intel Xeon E5520 2.2GHz",
-                    "memory": 32,
-                    "storage": "2x500GB SSD RAID1",
-                    "comment": "Retired offering, no new purchasing"
-                },
-                "hostname": "server1",
-                "rack": "B12",
-                "rackPosition": 4,
-                "owner": {
-                    "id": 1,
-                    "username": "lcollier",
-                    "displayName": "Lauren Collier",
-                    "email": "Lauren.Collier@duke.edu",
-                },
-                "comment": "Reserved for Duke project"
-            },
-            {
-                "id": 5,
-                "model": {
-                    "id": 1,
-                    "vendor": "Dell",
-                    "modelNumber": "R710",
-                    "height": 4,
-                    "displayColor": "#82E0AA",
-                    "ethernetPorts": 2,
-                    "powerPorts": 1,
-                    "cpu": "Intel Xeon E5520 2.2GHz",
-                    "memory": 32,
-                    "storage": "2x500GB SSD RAID1",
-                    "comment": "Retired offering, no new purchasing"
-                },
-                "hostname": "server1",
-                "rack": "B12",
-                "rackPosition": 4,
-                "owner": {
-                    "id": 1,
-                    "username": "adillon",
-                    "displayName": "Alex Dillon",
-                    "email": "Alex.Dillon@duke.edu",
-                },
-                "comment": "Reserved for Palaemon project"
             },
         ]
     },
-    {
-        "id": 2,
-        "address": "B13",
-        "rowLetter": "B",
-        "rackNumber": 13,
-        "instances": [
-            {
-                "id": 1,
-                "model": {
-                    "id": 1,
-                    "vendor": "Dell",
-                    "modelNumber": "R710",
-                    "height": 4,
-                    "displayColor": "#82E0AA",
-                    "ethernetPorts": 2,
-                    "powerPorts": 1,
-                    "cpu": "Intel Xeon E5520 2.2GHz",
-                    "memory": 32,
-                    "storage": "2x500GB SSD RAID1",
-                    "comment": "Retired offering, no new purchasing"
-                },
-                "hostname": "server1",
-                "rack": "B12",
-                "rackPosition": 4,
-                "owner": {
-                    "id": 1,
-                    "username": "bkeene",
-                    "displayName": "Brooke Keene",
-                    "email": "Brooke.Keene@duke.edu",
-                },
-                "comment": "Reserved for Palaemon project"
-            },
-            {
-                "id": 2,
-                "model": {
-                    "id": 2,
-                    "vendor": "Intel",
-                    "modelNumber": "C500",
-                    "height": 2,
-                    "displayColor": "#C39BD3",
-                    "ethernetPorts": 2,
-                    "powerPorts": 1,
-                    "cpu": "Intel Xeon E5520 2.2GHz",
-                    "memory": 32,
-                    "storage": "2x500GB SSD RAID1",
-                    "comment": "This is my personal favorite!",
-                    "instances": ["instance3", "instance4", "instance5"]
-                },
-                "hostname": "server2",
-                "rack": "B12",
-                "rackPosition": 12,
-                "owner": {
-                    "id": 1,
-                    "username": "grodriguez",
-                    "displayName": "Gaby Rodriguez-Florido",
-                    "email": "gr64@duke.edu",
-                },
-                "comment": "Reserved for 458 project"
-            },
-        ]
-    }
 ];
 
 function sortProperties(obj) {
