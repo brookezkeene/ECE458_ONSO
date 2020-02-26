1d2953cd516a925f3b701eb8ae60e56f6062612d
Brooke Keene <brooke.z.keene@gmail.com>
2020-02-26 07:18:02 -0500

    slight styling changes to login page

--- src/Web.Api/VueClient/src/components/Login.vue
+++ src/Web.Api/VueClient/src/components/Login.vue
@@ -6,7 +6,7 @@
         </v-row>
         
         <v-row justify="center">
-            <v-card width="50%">
+            <v-card width="50%" class="pa-10">
                 <v-card-title class="justify-center">
                     Log In
                 </v-card-title>
@@ -17,12 +17,15 @@
                         <v-container>
                             <v-row>
                                 <v-text-field v-model="username"
+                                              prepend-inner-icon="mdi-account"
                                               :rules="usernameRules"
                                               label="Username"
                                               required></v-text-field>
                             </v-row>
                             <v-row>
                                 <v-text-field v-model="password"
+                                              class="pb-5"
+                                              prepend-inner-icon="mdi-lock"
                                               :rules="passwordRules"
                                               :type="'password'"
                                               label="Password"
@@ -30,17 +33,20 @@
                             </v-row>
                         </v-container>
                     </v-form>
-                    <v-btn :disabled="!valid"
-                           color="primary"
-                           class="mr-4"
-                           type="submit"
-                           @click="submit">
-                        Sign In
-                    </v-btn>
-                    <v-spacer></v-spacer>
-                    <v-form action="/api/login/external" method="post">
-                        <v-btn type="submit">Sign in with Duke NetID</v-btn>
-                    </v-form>
+                    <v-card flat>
+                        <v-layout align-center justify-center>
+                            <v-btn :disabled="!valid"
+                                    color="primary"
+                                    type="submit"
+                                    @click="submit">
+                                Sign In
+                            </v-btn>
+                            <v-spacer></v-spacer>
+                            <v-form action="/api/login/external" method="post">
+                                <v-btn outlined color="primary" type="submit">Sign in with Duke NetID</v-btn>
+                            </v-form>
+                        </v-layout>
+                    </v-card>
                 </v-card-text>
             </v-card>
         </v-row>
