c61caf4bb05eb3a3a1933207098b2956ad8447b3
Brooke Keene <brooke.z.keene@gmail.com>
2020-02-26 18:18:09 -0500

    rack diagrams appear again and linked to asset detail views

--- src/Web.Api/Mappers/RackApiMapperProfile.cs
+++ src/Web.Api/Mappers/RackApiMapperProfile.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Web.Api.Common.Mappers;
 using Web.Api.Core.Dtos;
 using Web.Api.Dtos;
+using Web.Api.Dtos.Models.Read;
 using Web.Api.Dtos.Racks;
 
 namespace Web.Api.Mappers
@@ -19,6 +20,8 @@ namespace Web.Api.Mappers
             CreateMap<RackDto, GetRacksApiDto>()
                 .ReverseMap();
             CreateMap<AssetDto, GetRackAssetApiDto>()
+                .ReverseMap();
+            CreateMap<ModelDto, GetModelApiDto>()
                 .ReverseMap();
             CreateMap<PduDto, GetRackPdusApiDto>()
                 .ReverseMap();
