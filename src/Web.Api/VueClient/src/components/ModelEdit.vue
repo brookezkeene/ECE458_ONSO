4b0d1ef7b5cc7371f47a4ab678f83fec544c5766
Brooke Keene <brooke.z.keene@gmail.com>
2020-02-27 22:55:16 -0500

    found and fixed most model bugs

--- src/Web.Api/VueClient/src/components/ModelEdit.vue
+++ src/Web.Api/VueClient/src/components/ModelEdit.vue
@@ -18,8 +18,7 @@
                                         label="Vendor"
                                         placeholder="i.e. Dell" 
                                         :rules="[rules.vendorRules]"
-                                        counter="50"
-                                        required></v-combobox>
+                                        counter="50"></v-combobox>
                         </v-col>
                         <v-col cols="12" sm="6" md="4">
                             <v-text-field v-model="newItem.modelNumber" 
