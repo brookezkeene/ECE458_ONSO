dd97fce3089de60ed1efc1b400fc5ab567509732
Joyce <jyz11@duke.edu>
2020-03-24 21:12:08 -0400

    changed datedecommissioned to datetime migration

---
+++ src/Web.Api.Infrastructure/Migrations/20200325010804_DecommissionedAssetDateTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Api.Infrastructure.Migrations
+{
+    public partial class DecommissionedAssetDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "DecommissionedAssets");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDecommissioned",
+                table: "DecommissionedAssets",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDecommissioned",
+                table: "DecommissionedAssets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Date",
+                table: "DecommissionedAssets",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- src/Web.Api/Dtos/Assets/Create/CreateAssetApiDto.cs
+++ src/Web.Api/Dtos/Assets/Create/CreateAssetApiDto.cs
@@ -5,6 +5,7 @@ namespace Web.Api.Dtos.Assets.Create
 {
     public class CreateAssetApiDto : AssetApiDto
     {
+        public Guid? ChangePlanId { get; set; }
         public List<CreateAssetPowerPortApiDto> PowerPorts { get; set; }
         public List<CreateAssetNetworkPortApiDto> NetworkPorts { get; set; }
     }
