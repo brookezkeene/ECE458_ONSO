363186a916ca5688034d10d6c437db7f6d9f4df6
gabyr <gaby.rfp@gmail.com>
2020-03-25 01:16:00 -0400

    begin integrating change plan backend

---
+++ src/Web.Api/VueClient/src/repositories/changePlan.js
@@ -0,0 +1,12 @@
+﻿
+import axios from 'axios';
+const resource = '/changePlans';
+
+export default {
+    create(item) {
+        return axios.post(`${resource}`, item).then(response => response.data).catch(error => error);
+    },
+    createItem(item) {
+        return axios.post(`${resource}`, item).then(response => response.data).catch(error => error);
+    }
+}
