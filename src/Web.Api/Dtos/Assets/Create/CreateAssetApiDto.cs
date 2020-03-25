7178e4320e379aaab72f7ec5eee94e29595fe1d4
Joyce <jyz11@duke.edu>
2020-03-24 22:04:13 -0400

    rebased with ev3/sprint4

--- src/Web.Api/Dtos/Assets/Create/CreateAssetApiDto.cs
+++ src/Web.Api/Dtos/Assets/Create/CreateAssetApiDto.cs
@@ -5,7 +5,6 @@ namespace Web.Api.Dtos.Assets.Create
 {
     public class CreateAssetApiDto : AssetApiDto
     {
-        public Guid? ChangePlanId { get; set; }
         public List<CreateAssetPowerPortApiDto> PowerPorts { get; set; }
         public List<CreateAssetNetworkPortApiDto> NetworkPorts { get; set; }
     }
