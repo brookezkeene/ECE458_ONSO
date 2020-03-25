30dcceb72257ae06e41099861424e57baa6930e2
Joyce <jyz11@duke.edu>
2020-03-24 21:43:42 -0400

    passes all the tests

--- src/Web.Api/Dtos/Assets/Update/UpdateAssetApiDto.cs
+++ src/Web.Api/Dtos/Assets/Update/UpdateAssetApiDto.cs
@@ -5,8 +5,6 @@ namespace Web.Api.Dtos.Assets.Update
 {
     public class UpdateAssetApiDto : AssetApiDto
     {
-        public Guid? ChangePlanId { get; set; }
-
         public List<UpdateAssetPowerPortApiDto> PowerPorts { get; set; }
         public List<UpdateAssetNetworkPortApiDto> NetworkPorts { get; set; }
         public Guid Id { get; set; }
