d28f3779ddf65088773a52d075c6aacfc6557ec7
Cannon Palms <cannonpalms@me.com>
2020-03-29 19:54:30 -0400

    resolve trigger syntax issue with dynamically executed sql query in asset number sequence trigger

---
+++ src/Web.Api.Infrastructure/Migrations/ApplicationDb/20200329234442_FixDynamicSqlInAssetNumberTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Api.Infrastructure.Migrations.ApplicationDb
+{
+    public partial class FixDynamicSqlInAssetNumberTrigger : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP TRIGGER [update_asset_number_sequence]");
+            migrationBuilder.Sql(@"CREATE TRIGGER [update_asset_number_sequence]
+                                    ON [Assets]
+                                    AFTER INSERT
+                                    AS
+                                    BEGIN
+	                                    DECLARE @max_asset_number INT;
+	                                    DECLARE @s NVARCHAR(100);
+	                                    SET @max_asset_number = (SELECT ISNULL(MAX([AssetNumber]), 100000) FROM [Assets]);
+	                                    SET @s = N'ALTER SEQUENCE [AssetNumberSequence] RESTART WITH ' + CAST(@max_asset_number + 1 AS NVARCHAR(6));
+	                                    EXEC (@s);
+                                    END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP TRIGGER [update_asset_number_sequence]");
+            migrationBuilder.Sql(@"CREATE TRIGGER [update_asset_number_sequence]
+                                    ON [Assets]
+                                    FOR INSERT
+                                    AS
+                                    BEGIN
+	                                    DECLARE @max_asset_number INT;
+	                                    DECLARE @s NVARCHAR(100);
+	                                    SET @max_asset_number = (SELECT MAX([AssetNumber]) FROM [Assets]);
+	                                    SET @s = N'
+		                                    ALTER SEQUENCE
+			                                    [AssetNumberSequence]
+		                                    RESTART WITH ' + CAST(@max_asset_number + 1 AS NVARCHAR(6));
+	                                    EXEC (@s);
+                                    END");
+        }
+    }
+}
