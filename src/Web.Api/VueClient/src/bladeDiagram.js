b45f3b38fd4cf575edac1db230f3a875d4515ecb
Brooke Keene <brooke.z.keene@gmail.com>
2020-04-11 22:22:24 -0400

    created mock data for blade diagrams

---
+++ src/Web.Api/VueClient/src/bladeDiagram.js
@@ -0,0 +1,368 @@
+﻿const bladeChassis1 = {
+    "slotsOccupied": [
+        0
+    ],
+    "powerPorts": [
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "number": 0,
+            "pduPort": "string",
+            "pduPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        }
+    ],
+    "networkPorts": [
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "name": "string",
+            "number": 0,
+            "connectedPort": {
+                "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                "name": "string",
+                "hostname": "string",
+                "assetId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                "macAddress": "string",
+                "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+            },
+            "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "macAddress": "string",
+            "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+        }
+    ],
+    "rack": "string",
+    "height": 0,
+    "vendor": "string",
+    "modelNumber": "string",
+    "displayColor": "string",
+    "owner": "string",
+    "datacenterId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "datacenter": "string",
+    "hasNetworkManagedPower": true,
+    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "lastUpdatedDate": "2020-04-12T01:02:04.201Z",
+    "changePlanId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "rackId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "hostname": "string",
+    "comment": "string",
+    "rackPosition": 0,
+    "ownerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "modelId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "assetNumber": 0,
+    "blades": [
+        {
+            "slotsOccupied": [
+                0
+            ],
+            "powerPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "number": 0,
+                    "pduPort": "string",
+                    "pduPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "networkPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "name": "string",
+                    "number": 0,
+                    "connectedPort": {
+                        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "name": "string",
+                        "hostname": "string",
+                        "assetId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "macAddress": "string",
+                        "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                    },
+                    "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "macAddress": "string",
+                    "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "rack": "string",
+            "height": 0,
+            "vendor": "string",
+            "modelNumber": "string",
+            "displayColor": "string",
+            "owner": "string",
+            "datacenterId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "datacenter": "string",
+            "hasNetworkManagedPower": true,
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "lastUpdatedDate": "2020-04-12T01:02:04.201Z",
+            "changePlanId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "chassisId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "hostname": "string",
+            "comment": "string",
+            "slot": 1,
+            "ownerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "modelId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "assetNumber": 0,
+        },
+        {
+            "slotsOccupied": [
+                0
+            ],
+            "powerPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "number": 0,
+                    "pduPort": "string",
+                    "pduPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "networkPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "name": "string",
+                    "number": 0,
+                    "connectedPort": {
+                        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "name": "string",
+                        "hostname": "string",
+                        "assetId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "macAddress": "string",
+                        "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                    },
+                    "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "macAddress": "string",
+                    "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "rack": "string",
+            "height": 0,
+            "vendor": "string",
+            "modelNumber": "string",
+            "displayColor": "string",
+            "owner": "string",
+            "datacenterId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "datacenter": "string",
+            "hasNetworkManagedPower": true,
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "lastUpdatedDate": "2020-04-12T01:02:04.201Z",
+            "changePlanId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "chassisId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "hostname": "string",
+            "comment": "string",
+            "slot": 4,
+            "ownerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "modelId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "assetNumber": 0,
+        },
+        {
+            "slotsOccupied": [
+                0
+            ],
+            "powerPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "number": 0,
+                    "pduPort": "string",
+                    "pduPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "networkPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "name": "string",
+                    "number": 0,
+                    "connectedPort": {
+                        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "name": "string",
+                        "hostname": "string",
+                        "assetId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "macAddress": "string",
+                        "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                    },
+                    "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "macAddress": "string",
+                    "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "rack": "string",
+            "height": 0,
+            "vendor": "string",
+            "modelNumber": "string",
+            "displayColor": "string",
+            "owner": "string",
+            "datacenterId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "datacenter": "string",
+            "hasNetworkManagedPower": true,
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "lastUpdatedDate": "2020-04-12T01:02:04.201Z",
+            "changePlanId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "chassisId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "hostname": "string",
+            "comment": "string",
+            "slot": 5,
+            "ownerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "modelId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "assetNumber": 0,
+        },
+        {
+            "slotsOccupied": [
+                0
+            ],
+            "powerPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "number": 0,
+                    "pduPort": "string",
+                    "pduPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "networkPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "name": "string",
+                    "number": 0,
+                    "connectedPort": {
+                        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "name": "string",
+                        "hostname": "string",
+                        "assetId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "macAddress": "string",
+                        "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                    },
+                    "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "macAddress": "string",
+                    "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "rack": "string",
+            "height": 0,
+            "vendor": "string",
+            "modelNumber": "string",
+            "displayColor": "string",
+            "owner": "string",
+            "datacenterId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "datacenter": "string",
+            "hasNetworkManagedPower": true,
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "lastUpdatedDate": "2020-04-12T01:02:04.201Z",
+            "changePlanId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "chassisId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "hostname": "string",
+            "comment": "string",
+            "slot": 7,
+            "ownerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "modelId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "assetNumber": 0,
+        },
+        {
+            "slotsOccupied": [
+                0
+            ],
+            "powerPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "number": 0,
+                    "pduPort": "string",
+                    "pduPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "networkPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "name": "string",
+                    "number": 0,
+                    "connectedPort": {
+                        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "name": "string",
+                        "hostname": "string",
+                        "assetId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "macAddress": "string",
+                        "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                    },
+                    "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "macAddress": "string",
+                    "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "rack": "string",
+            "height": 0,
+            "vendor": "string",
+            "modelNumber": "string",
+            "displayColor": "string",
+            "owner": "string",
+            "datacenterId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "datacenter": "string",
+            "hasNetworkManagedPower": true,
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "lastUpdatedDate": "2020-04-12T01:02:04.201Z",
+            "changePlanId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "chassisId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "hostname": "string",
+            "comment": "string",
+            "slot": 10,
+            "ownerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "modelId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "assetNumber": 0,
+        },
+        {
+            "slotsOccupied": [
+                0
+            ],
+            "powerPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "number": 0,
+                    "pduPort": "string",
+                    "pduPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "networkPorts": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "name": "string",
+                    "number": 0,
+                    "connectedPort": {
+                        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "name": "string",
+                        "hostname": "string",
+                        "assetId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "macAddress": "string",
+                        "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                    },
+                    "modelNetworkPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "macAddress": "string",
+                    "connectedPortId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+                }
+            ],
+            "rack": "string",
+            "height": 0,
+            "vendor": "string",
+            "modelNumber": "string",
+            "displayColor": "string",
+            "owner": "string",
+            "datacenterId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "datacenter": "string",
+            "hasNetworkManagedPower": true,
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "lastUpdatedDate": "2020-04-12T01:02:04.201Z",
+            "changePlanId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "rackId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "hostname": "string",
+            "comment": "string",
+            "slot": 12,
+            "ownerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "modelId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "assetNumber": 0,
+        },
+    ]
+};
+
+export default {
+    /* Function to create text and styling for each blade */
+    createSlots() {
+        /*eslint-disable*/
+        console.log("createSlot called")
+        return
+    }
+}
