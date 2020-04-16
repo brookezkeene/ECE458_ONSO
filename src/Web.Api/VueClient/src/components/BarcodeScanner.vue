083d58ee979a0242df6355613a56e1a2c9d22983
gabyr <gaby.rfp@gmail.com>
2020-04-15 22:34:42 -0400

    integrated barcode scanner library into page

--- src/Web.Api/VueClient/src/components/BarcodeScanner.vue
+++ src/Web.Api/VueClient/src/components/BarcodeScanner.vue
@@ -1,20 +1,38 @@
-﻿
-<template>
-    <v-card flat>
-        <v-card-title>Asset Barcode Scanner</v-card-title>
-    </v-card>
-
+﻿<template>
+    <div>
+        <v-card flat>
+            <v-card-title>Asset Label Scanner</v-card-title>
+            <v-btn @click="scan=!scan" color="primary" class="center mb-2" :depressed="scan">Click to begin scanning</v-btn>
+            <v-quagga v-if="scan===true" :onDetected="logIt" :readerSize="readerSize" :readerTypes="['ean_reader']"></v-quagga>
+        </v-card>
+    </div>
 </template>
 
-
 <script>
+    import Vue from 'vue'
+    import VueQuagga from 'vue-quaggajs';
+
+    // register component 'v-quagga'
+    Vue.use(VueQuagga);
+
     export default {
-        name: 'barcode-scanner',
+        name: 'VueBarcodeTest',
         data() {
             return {
-
+                readerSize: {
+                    width: 640,
+                    height: 480
+                },
+                detecteds: [],
+                scan: false
             }
-        }
+        },
+        methods: {
+            logIt(data) {
+                /*eslint-disable*/
+                console.log('detected', data)
+            }
 
+        }
     }
 </script>
