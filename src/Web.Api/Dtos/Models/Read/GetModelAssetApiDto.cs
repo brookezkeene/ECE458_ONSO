d4029c7f8384a1ce3e4d0328ee9217cf49f87696
gabyr <gaby.rfp@gmail.com>
2020-04-17 20:36:53 -0400

    fix asset number not showing up in model details page

--- src/Web.Api/Dtos/Models/Read/GetModelAssetApiDto.cs
+++ src/Web.Api/Dtos/Models/Read/GetModelAssetApiDto.cs
@@ -5,6 +5,7 @@ namespace Web.Api.Dtos.Models.Read
     public class GetModelAssetApiDto
     {
         public Guid Id { get; set; }
-        public string Hostname { get; set; }
+        public string Hostname { get; set; } 
+        public int AssetNumber { get; set; }
     }
 }
